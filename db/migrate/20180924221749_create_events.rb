e0e7a0fd9b71194c449b2fd4c685b19a0cdf41e7
jabetancourtb <jabetancourtb@hotmail.com>
2018-10-02 14:09:02 -0500

    Usando Postgrades

--- db/migrate/20180924221749_create_events.rb
+++ db/migrate/20180924221749_create_events.rb
@@ -4,7 +4,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.string :event_name
       t.string :description
       t.date :date
-      t.datatime :time
+      t.datetime :time
       t.string :avg_rating
       t.string :category
       t.string :latitude
